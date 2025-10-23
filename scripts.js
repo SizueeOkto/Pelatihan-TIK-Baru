ce3f1e307bc084212692cb552acc2b636164db5c
SizueeOkto <unggul20octo@gmail.com>
2025-10-23 14:34:25 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
-//1.  Pilih tombol berdassarkan ID-nya
-const tombolSapa = document.getElementByld('sapaButton'):
-//2.  Tambahkaan'event listener untuk aksi 'click'
-tombolSapa.addEventListener('click',function(){
-  //3.  Jalankan kode ini ketika tombol di-klik
-  alert('Halo! Terima kasih sudah berkunjung!');
+// 1. Pilih tombol berdasarkan ID-nya
+const tombolSapa = document.getElementById('sapaButton');
+
+// 2. Tambahkan event listener untuk aksi 'click'
+tombolSapa.addEventListener('click', function() {
+    // 3. Jalankan kode ini ketika tombol di-klik
+    alert('Halo! Terima kasih sudah berkunjung!');
 });
