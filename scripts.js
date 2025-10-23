433aa44530467fed06c17d154f44248c02fdc88e
SizueeOkto <unggul20octo@gmail.com>
2025-10-23 14:26:58 +0700

    Add greeting button functionality in scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+//1.  Pilih tombol berdassarkan ID-nya
+const tombolSapa = document.getElementByld('sapaButton'):
+//2.  Tambahkaan'event listener untuk aksi 'click'
+tombolSapa.addEventListener('click',function(){
+  //3.  Jalankan kode ini ketika tombol di-klik
+  alert('Halo! Terima kasih sudah berkunjung!');
+});
